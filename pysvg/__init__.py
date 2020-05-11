7c24cb6c3c92dd7fcd77ed67d2019f629c6f3d4b
moritzleidinger <moritz.leidinger@icloud.com>
2020-05-11 18:15:39 +0200

    add pysvg lib

---
+++ pysvg/__init__.py
@@ -0,0 +1,23 @@
+from .animate import *
+from .attributes import *
+from .builders import *
+from .core import *
+from .filter import *
+from .gradient import *
+from .linking import *
+from .parser import *
+from .script import *
+from .shape import *
+from .structure import *
+from .style import *
+from .text import *
+from .turtle import *
+from .util import *
+
+# __all__ = []
+# for subpackage in ['core', 'filter', 'gradient', 'linking', 'script','shape','structure','style','text']:
+#    try:
+#        exec 'import ' + subpackage
+#        __all__.append( subpackage )
+#    except ImportError:
+#        pass
